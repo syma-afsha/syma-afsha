91c874d6362dfa85573146d32a56bc35be98ff82
Syma Afsha <symaafsha.eece@gmail.com>
2024-06-23 19:27:14 +0200

    -o

--- symaafsha/src/rest.js
+++ symaafsha/src/rest.js
@@ -13,9 +13,9 @@ export default function Rest(){
 
                       <p style={{color: "white", fontFamily: "serif", fontSize:19}}>To follow my work at <a  style={{ color: "#FFFF00"}} href="https://www.researchgate.net/profile/Syma-Afsha" target="_blank">ResearchGate</a>.</p>
                   <Row>
-                  <Col xs={4} sm={4} lg={3} >
+                  <Col xs={6} sm={6} lg={3} >
             <Link style={{color: "white", textDecoration: "none"}} to ="/about-me"><Button variant="outline-primary" style={{color:"white", fontSize: 18, textAlign: "center", width: 150, height: 50, fontFamily: "serif"}}className="home-button">Read More</Button></Link></Col>
-            <Col xs={4} sm={4} lg={3} >
+            <Col xs={6} sm={6} lg={3} >
             <a style={{color: "white", textDecoration: "none"}} href="https://drive.google.com/file/d/129mDal8P_cEGxH5xxcqBfxBRQvDptgJE/view?usp=sharing" target="_blank"> <Button variant="outline-primary" style={{color:"white", fontSize: 18, textAlign: "center", width: 150, height: 50, fontFamily: "serif", marginLeft: 25}}className="home-button">My Resume</Button></a></Col>
             
                   </Row>
