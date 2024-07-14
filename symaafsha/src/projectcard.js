9dd21180bfc775cf3eadf68b3d5ff1dea873c512
Syma Afsha <symaafsha.eece@gmail.com>
2024-07-14 15:12:19 +0200

    ---------------

--- symaafsha/src/projectcard.js
+++ symaafsha/src/projectcard.js
@@ -2,16 +2,19 @@ import React from 'react';
 
 import './project.css'; // Import your CSS here
 function ProjectCard({ title, description, videoUrl, imageUrl }) {
+    const [showVideo, setShowVideo] = useState(true);
+
     return (
-        <div className="card">
+        <div className="card" onClick={() => setShowVideo(!showVideo)}>
             <div className="media-container">
-                {videoUrl && (
+                {showVideo ? (
                     <video controls className="media-video">
                         <source src={videoUrl} type="video/mp4" />
                         Your browser does not support the video tag.
                     </video>
+                ) : (
+                    <img src={imageUrl} alt={title} className="media-image" />
                 )}
-                {imageUrl && <img src={imageUrl} alt={title} className="media-image" />}
             </div>
             <div className="content">
                 <h3>{title}</h3>
