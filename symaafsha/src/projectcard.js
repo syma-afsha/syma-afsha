d2e93b38c03f855641af5148ea9a03254bb87a4e
Syma Afsha <symaafsha.eece@gmail.com>
2024-07-14 15:57:16 +0200

    ---

--- symaafsha/src/projectcard.js
+++ symaafsha/src/projectcard.js
@@ -4,24 +4,21 @@ import './project.css'; // Import your CSS here
 function ProjectCard({ title, imageUrl, videoUrl }) {
     const [videoVisible, setVideoVisible] = useState(false);
 
-    const handleVideoButton = () => {
-        // This function could also open a modal or a new window with the video
-        setVideoVisible(!videoVisible);
-    };
+    const toggleVideo = () => setVideoVisible(!videoVisible);
 
     return (
         <div className="card">
             <img src={imageUrl} alt={title} className="card-image" />
-            <div className="card-content">
+            <div className="card-description">
                 <h3>{title}</h3>
-                <button onClick={handleVideoButton} className="video-button">
-                    {videoVisible ? "Hide Video" : "Watch Video"}
+                <button onClick={toggleVideo} className="toggle-video">
+                    {videoVisible ? 'Hide Video' : 'Watch Video'}
                 </button>
             </div>
             {videoVisible && (
-                <video controls className="card-video">
+                <video controls src={videoUrl} className="card-video">
                     <source src={videoUrl} type="video/mp4" />
-                  
+                    
                 </video>
             )}
         </div>
