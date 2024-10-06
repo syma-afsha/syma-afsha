39444f02880157fe8a3db8b41c2af99df5a89fc6
Syma Afsha <symaafsha.eece@gmail.com>
2024-10-07 00:40:12 +0200

    Updates

---
+++ precache-manifest.35f10e3d1f76f7c35e322839bbb9853f.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "342a7388a10199522c89a57163ff57f7",
+    "url": "/syma-afsha/index.html"
+  },
+  {
+    "revision": "a61197d4a98683284d49",
+    "url": "/syma-afsha/static/css/2.970e759d.chunk.css"
+  },
+  {
+    "revision": "fc1f1b5da91318b0c568",
+    "url": "/syma-afsha/static/css/main.f08465fa.chunk.css"
+  },
+  {
+    "revision": "a61197d4a98683284d49",
+    "url": "/syma-afsha/static/js/2.ee1ba75e.chunk.js"
+  },
+  {
+    "revision": "fc1f1b5da91318b0c568",
+    "url": "/syma-afsha/static/js/main.5eca2b73.chunk.js"
+  },
+  {
+    "revision": "bdaecfbbc82e7fb3b3f2",
+    "url": "/syma-afsha/static/js/runtime~main.aafb30e0.js"
+  },
+  {
+    "revision": "f52121b089fe796d205764f21278bdd7",
+    "url": "/syma-afsha/static/media/Aruco_Stonefish.f52121b0.png"
+  },
+  {
+    "revision": "8ea01f847d470aaf706f0f6bd93b7e97",
+    "url": "/syma-afsha/static/media/CVIcon.8ea01f84.png"
+  },
+  {
+    "revision": "4af28ef4d1288f09b9b5b53f6ad7361f",
+    "url": "/syma-afsha/static/media/MIST.4af28ef4.png"
+  },
+  {
+    "revision": "f22aee3c3698d5036b72ec1c3d749450",
+    "url": "/syma-afsha/static/media/P2.f22aee3c.png"
+  },
+  {
+    "revision": "2dc228ec4ed849d83cf58e3e310afc46",
+    "url": "/syma-afsha/static/media/badge(1).2dc228ec.png"
+  },
+  {
+    "revision": "3efc4fff35fc7a19475cfa4f3e05993b",
+    "url": "/syma-afsha/static/media/badge.3efc4fff.png"
+  },
+  {
+    "revision": "8fe121a5e9b271599f02ec9b01f48ec0",
+    "url": "/syma-afsha/static/media/cap.8fe121a5.png"
+  },
+  {
+    "revision": "6c8bf35ce645a8ca99a65ea987911759",
+    "url": "/syma-afsha/static/media/cv.6c8bf35c.png"
+  },
+  {
+    "revision": "4e86375cca60e6d0f9a9ea512b92977b",
+    "url": "/syma-afsha/static/media/erusmus.4e86375c.png"
+  },
+  {
+    "revision": "fd3bcfea2c3f4aec380d2a8474df080c",
+    "url": "/syma-afsha/static/media/flowchart-new.fd3bcfea.png"
+  },
+  {
+    "revision": "9cbdd7998446fd02896165833be1f97f",
+    "url": "/syma-afsha/static/media/footer-mail.9cbdd799.png"
+  },
+  {
+    "revision": "7ca5ba306b8e79c6db450d0623f155c3",
+    "url": "/syma-afsha/static/media/github.7ca5ba30.png"
+  },
+  {
+    "revision": "f1a0d2b1f86d59c2f3266dc085c589f4",
+    "url": "/syma-afsha/static/media/googlescholar.f1a0d2b1.png"
+  },
+  {
+    "revision": "0288effc35fae4cc8c74b584b3be80e9",
+    "url": "/syma-afsha/static/media/googlescholaricon.0288effc.png"
+  },
+  {
+    "revision": "13304f8dd8b7f8b13680db81709f8a22",
+    "url": "/syma-afsha/static/media/iceeict.13304f8d.png"
+  },
+  {
+    "revision": "a52927a0fb405deb59294065eecf4ca4",
+    "url": "/syma-afsha/static/media/iot_website_view.a52927a0.png"
+  },
+  {
+    "revision": "41855caee564ada068d60ed6f38c8f64",
+    "url": "/syma-afsha/static/media/pdf.41855cae.png"
+  },
+  {
+    "revision": "b71c2b65f9a3d8413070a0dbf46d04b1",
+    "url": "/syma-afsha/static/media/pick.b71c2b65.png"
+  },
+  {
+    "revision": "2113f20bdac9d22345e9a04c8db707f4",
+    "url": "/syma-afsha/static/media/re.2113f20b.png"
+  },
+  {
+    "revision": "d1fccc41e378350a35b5ce62ed56c5bf",
+    "url": "/syma-afsha/static/media/re1.d1fccc41.png"
+  },
+  {
+    "revision": "5037759c51b291c4ce91d421f1f2f520",
+    "url": "/syma-afsha/static/media/rose.5037759c.png"
+  },
+  {
+    "revision": "367e2b96be7d44c647d593a53c65692c",
+    "url": "/syma-afsha/static/media/signpaper.367e2b96.jpg"
+  },
+  {
+    "revision": "bf060ee344d01a29a7f7ab07274f9b1e",
+    "url": "/syma-afsha/static/media/slam.bf060ee3.png"
+  },
+  {
+    "revision": "12868d0c5b4ad585c9584539c2bcdb3d",
+    "url": "/syma-afsha/static/media/violent.12868d0c.png"
+  }
+]);
