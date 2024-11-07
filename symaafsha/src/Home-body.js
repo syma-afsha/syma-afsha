a0a6a49623bc4de67e5cb901c5a17891de00f258
Syma Afsha <symaafsha.eece@gmail.com>
2024-11-07 22:15:22 +0100

    --

--- symaafsha/src/Home-body.js
+++ symaafsha/src/Home-body.js
@@ -14,7 +14,7 @@ export default function HomeBody(){
       
       <Row>
 
-        <Col xs={12} md={8} lg={8} >
+        <Col xs={12} md={7} lg={7} >
 
           <Card className="home-card" style={{backgroundColor: "black"}} >
             <Card.Img
@@ -27,7 +27,7 @@ export default function HomeBody(){
 
         </Col>
        
-        <Col xs={12} md={4} lg={4} >
+        <Col xs={12} md={3} lg={3} >
          
 <Container>
             <Card.Body className='body-text'>
